d6b46c984f42ce3eea948f053d6dc69bf8db3af7
Tejas Patil <tejaspatil@Tejass-MacBook-Air.local>
2024-09-29 04:09:53 +0530

    useRef Practice

--- src/components/Hooks/useRef/useRef.jsx
+++ src/components/Hooks/useRef/useRef.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const UseRefDemo = () => {
+  const [name, setName] = useState('Tejas')
+  const refElement = useRef()
+
+  const handleReset = () => {
+    setName('Tejas')
+    refElement.current.focus()
+    // console.log(refElement)
+    // refElement.current.style.color='blue'
+  }
   return (
-    <div style={{margin: '2rem'}}>
+    <div style={{ margin: '2rem' }}>
       <h1>useRef</h1>
       <p><Link to={'https://www.youtube.com/watch?v=nX9ShZo0COc'}> Reference</Link></p>
+      <input ref={refElement} value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
