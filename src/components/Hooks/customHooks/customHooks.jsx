6b549365cfdc9d9b17191b29a937242b52cb6d4e
Tejas Patil <tejaspatil@Tejass-MacBook-Air.local>
2024-09-29 04:48:04 +0530

    Custom hook Practice

--- src/components/Hooks/customHooks/customHooks.jsx
+++ src/components/Hooks/customHooks/customHooks.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useCounter from './useCounter';
 
 const CustomHookDemo = () => {
+  const [count, Increment, Decrement] = useCounter()
   return (
     <div style={{ margin: '2rem' }}>
       <h1>Custom Hook</h1>
       <p><Link to={'https://www.youtube.com/watch?v=Vs43P8zcRr4'}> Reference</Link></p>
+      <div>
+        <button onClick={Decrement}>-</button>
+        <input value={count} />
+        <button onClick={Increment}>+</button>
+      </div>
     </div>
   );
 }
